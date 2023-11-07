a489babaaa0e08ce5bed72b98fe100cd17dc1774
cleo.eiras <cleo.eiras@etu.uca.fr>
2023-11-07 07:01:17 +0100

    :construction: upgrade of the navbar
    TODO :  the navbar shouldn't be over the projectspage before scrolling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,11 +4,20 @@ const Navbar = () => {
     const scrolled = useScrollTop();
 
     return (
-        <div className={"max-w-screen pr-8 w-full p-4 flex justify-end gap-10 rounded" + (scrolled ? 'bg-red' : 'bg-red-200')}>
+        <div
+            className={`max-w-screen bg-red-200 font-bodytxt pr-8 w-full p-4 flex justify-end gap-10 ${
+                scrolled ? '' : 'bg-red-50'
+            } rounded`}
+            style={{
+                position: 'fixed',
+                top: 0,
+                zIndex: 1000 // Pour s'assurer que la barre de navigation est au-dessus des autres éléments
+            }}
+        >
             <p>Github</p>
             <p>LinkedIn</p>
         </div>
-    )
+    );
 };
 
 export default Navbar;
