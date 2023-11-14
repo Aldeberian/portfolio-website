99400fc801c3495419a74b0d50aeed2430ae5860
cleo.eiras <cleo.eiras@etu.uca.fr>
2023-11-14 08:28:46 +0100

    :lipstick: :bug: Visual bugfix

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -13,7 +13,7 @@ const ProjectsPage = () => {
             <Title/>
             <div className="flex-1 flex flex-wrap justify-center">
                 {Projects.map((project) => (
-                    <div key={project.title} onClick={() => projectClick(project.link)} className="text-gray-300 box m-2 bg-csprim flex flex-col flex-nowrap rounded-xl lg:w-1/4 min-w-[300px] h-[200px]">
+                    <div key={project.title} onClick={() => projectClick(project.link)} className="text-gray-300 box m-2 bg-csprim flex flex-col flex-nowrap rounded-xl lg:w-1/4 overflow-scroll w-[300px] h-[200px]">
                         <p className="w-full p-5 text-center font-rubiktitle uppercase">{project.title}</p>
                         <p className="flex-1 m-3 font-rubiktitle">{project.description}</p>
                         <div className="flex flex-row justify-end gap-1 pr-2 pb-1">
